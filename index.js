95771f6cfb99ea23e31e52872688e5b00d41d338
widiskel <waputra16@gmail.com>
2024-07-18 15:28:33 +0700

    fix (login) add handle when login error

--- index.js
+++ index.js
@@ -14,94 +14,103 @@ async function operation(user, query, url) {
     console.log(err);
     process.exit();
   });
-  await Helper.sleep(2000, user, `Connected to Tap Swap`, tapswap);
+  if (tapswap.access_token) {
+    await Helper.sleep(2000, user, `Connected to Tap Swap`, tapswap);
 
-  while (
-    tapswap.player.charge_level != 5 &&
-    (await tapswap.upgrade("charge")) == true
-  ) {}
-  while (
-    tapswap.player.energy_level != 20 &&
-    (await tapswap.upgrade("energy")) == true
-  ) {}
-  while (
-    tapswap.player.tap_level != 20 &&
-    (await tapswap.upgrade("tap")) == true
-  ) {}
+    while (
+      tapswap.player.charge_level != 5 &&
+      (await tapswap.upgrade("charge")) == true
+    ) {}
+    while (
+      tapswap.player.energy_level != 20 &&
+      (await tapswap.upgrade("energy")) == true
+    ) {}
+    while (
+      tapswap.player.tap_level != 20 &&
+      (await tapswap.upgrade("tap")) == true
+    ) {}
 
-  const tap = Math.floor(tapswap.player.energy / tapswap.player.tap_level);
-  await tapswap.submitTap(tap);
-  while ((await tapswap.applyBoost("energy")) == true) {
     const tap = Math.floor(tapswap.player.energy / tapswap.player.tap_level);
     await tapswap.submitTap(tap);
-  }
-  while ((await tapswap.applyBoost("turbo")) == true) {
-    await Helper.sleep(
-      15000,
-      user,
-      `Delaying for 15 Second before submitting tap with booster`,
-      tapswap
-    );
-    const tap = Math.floor(Helper.random(400, 800));
-    await tapswap.submitTap(tap);
-    await Helper.sleep(
-      5000,
-      user,
-      `Tap with booster submitted, delaying for 5 Second until boost end`,
-      tapswap
-    );
-  }
-
-  const onGoingMission = [];
-  for (const missions of tapswap.config.missions) {
-    if (
-      !tapswap.account.missions.completed.includes(missions.id) &&
-      !tapswap.account.missions.active.includes(missions.id)
-    ) {
-      await tapswap.joinMissions(missions.id);
-      await tapswap.finishMissionsItem(missions.id);
-      onGoingMission.push(missions.id);
+    while ((await tapswap.applyBoost("energy")) == true) {
+      const tap = Math.floor(tapswap.player.energy / tapswap.player.tap_level);
+      await tapswap.submitTap(tap);
     }
-  }
-
-  if (tapswap.account.missions.active.length > 0) {
-    await Helper.sleep(
-      60000 * 6,
-      user,
-      `Delaying for 6 Min before completing missions`,
-      tapswap
-    );
-
-    for (const missions of tapswap.account.missions.active) {
-      const miss = tapswap.config.missions.filter(
-        (item) => item.id == missions.id
+    while ((await tapswap.applyBoost("turbo")) == true) {
+      await Helper.sleep(
+        15000,
+        user,
+        `Delaying for 15 Second before submitting tap with booster`,
+        tapswap
       );
+      const tap = Math.floor(Helper.random(400, 800));
+      await tapswap.submitTap(tap);
+      await Helper.sleep(
+        5000,
+        user,
+        `Tap with booster submitted, delaying for 5 Second until boost end`,
+        tapswap
+      );
+    }
 
-      if (miss.length != 0) {
-        if (miss[0].items[0].require_answer == true) {
-          const missionInput = MISSIONS[missions.id];
-          if (missionInput != undefined) {
-            await tapswap.finishMissionsItemWithInput(
-              missions.id,
-              missionInput
-            );
-          }
-        }
-        await tapswap.finishMissions(missions.id);
-        await tapswap.claimMission(missions.id);
+    const onGoingMission = [];
+    for (const missions of tapswap.config.missions) {
+      if (
+        !tapswap.account.missions.completed.includes(missions.id) &&
+        !tapswap.account.missions.active.includes(missions.id)
+      ) {
+        await tapswap.joinMissions(missions.id);
+        await tapswap.finishMissionsItem(missions.id);
+        onGoingMission.push(missions.id);
       }
     }
-  }
 
-  await tapswap.browser.close();
-  await Helper.sleep(
-    60000 * 10,
-    user,
-    `Account ${user.firstName + " " + user.lastName}(${
-      user.id
-    }) Processing Complete, Delaying for 10 Minutes`,
-    tapswap
-  );
+    if (tapswap.account.missions.active.length > 0) {
+      await Helper.sleep(
+        60000 * 6,
+        user,
+        `Delaying for 6 Min before completing missions`,
+        tapswap
+      );
+
+      for (const missions of tapswap.account.missions.active) {
+        const miss = tapswap.config.missions.filter(
+          (item) => item.id == missions.id
+        );
+
+        if (miss.length != 0) {
+          if (miss[0].items[0].require_answer == true) {
+            const missionInput = MISSIONS[missions.id];
+            if (missionInput != undefined) {
+              await tapswap.finishMissionsItemWithInput(
+                missions.id,
+                missionInput
+              );
+            }
+          }
+          await tapswap.finishMissions(missions.id);
+          await tapswap.claimMission(missions.id);
+        }
+      }
+    }
+
+    await tapswap.browser.close();
+    await Helper.sleep(
+      60000 * 10,
+      user,
+      `Account ${user.firstName + " " + user.lastName}(${
+        user.id
+      }) Processing Complete, Delaying for 10 Minutes`,
+      tapswap
+    );
+  } else {
+    await Helper.sleep(
+      60000 * 5,
+      user,
+      `Cannot Login, Posible application is overloaded, retrying aver 5 minutes`,
+      tapswap
+    );
+  }
   await operation(user, query, url);
 }
 
