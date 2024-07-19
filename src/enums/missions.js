e01abc6a67d46779d9af9fcd9f098f91beff554c
widiskel <waputra16@gmail.com>
2024-07-20 02:36:30 +0700

    feat (missions) add more missions to mission enums

--- src/enums/missions.js
+++ src/enums/missions.js
@@ -2,9 +2,12 @@ export const MISSIONS = {
   M2012: "infinite",
   M2013: "parachain",
   M2014: "instamine",
+  M2015: "WebSocket",
   M1015: "D5784VHPC377",
   M1016: "5SP670KR66",
   M1017: "D772WQ9Z5",
+  M1018: "7N008TQ31V",
+  M1019: "0TJN63R97A",
 
   getInput: function (value) {
     for (const key in this) {
